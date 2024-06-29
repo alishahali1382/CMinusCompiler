ad8449b1c867c91f870601712d2442de18eceb24
alishahali1382 <alishahali1382@gmail.com>
2024-06-29 18:10:53 +0330

    edit compiler.py so that the printed output is exactly like the tests output (debugging use only)

--- src/compiler.py
+++ src/compiler.py
@@ -14,4 +14,4 @@ parser.parse_and_write()
 print("\n")
 for i, line in enumerate(parser.codegen.PB):
     if line != None:
-        print(f"{i}: ({', '.join((str(x) if x is not None else '') for x in line)})")
+        print(f"{i}\t({', '.join((str(x) if x is not None else ' ') for x in line)} )")

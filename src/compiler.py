5acd8716c7bb7d6b9a881675f3d9b655fbbe9424
alishahali1382 <alishahali1382@gmail.com>
2024-07-01 01:05:41 +0330

    write semantic errors to file

--- src/compiler.py
+++ src/compiler.py
@@ -11,19 +11,28 @@ sys.setrecursionlimit(1000)
 parser = Parser(grammar_rules)
 parser.parse_and_write()
 
-print("\n")
-with open("output.txt", "w") as f:
-    lastline = -1
-    for i, line in enumerate(parser.codegen.PB):
-        if line != None:
-            code = f"{i}\t({', '.join((str(x) if x is not None else ' ') for x in line)} )"
-            #assert lastline == i-1, f"Line {i} is not in order, last line was {lastline}"
-            code = "\n"*(i-lastline-1) + code
-            lastline = i
-            f.write(code + "\n")
-            if parser.codegen.comment[i] != None:
-                code = f"{code:<40}" #{parser.codegen.comment[i]}
-            print(code)
-
 with open("semantic_errors.txt", "w") as f:
-    f.write("The input program is semantically correct.\n")
+    has_errors = len(parser.codegen.semantic_errors) > 0
+    if has_errors:
+        for error in parser.codegen.semantic_errors:
+            f.write(error + "\n")
+    else:
+        f.write("The input program is semantically correct.\n")
+
+# print("\n")
+with open("output.txt", "w") as f:
+    if has_errors:
+        f.write("The code has not been generated.\n")
+    else:
+        lastline = -1
+        for i, line in enumerate(parser.codegen.PB):
+            if line != None:
+                code = f"{i}\t({', '.join((str(x) if x is not None else ' ') for x in line)} )"
+                #assert lastline == i-1, f"Line {i} is not in order, last line was {lastline}"
+                code = "\n"*(i-lastline-1) + code
+                lastline = i
+                f.write(code + "\n")
+                if parser.codegen.comment[i] != None:
+                    code = f"{code:<40}" #{parser.codegen.comment[i]}
+                print(code)
+
